ccb7ee9daefc0530d128a5693cdc8c91dbbc64ab
n221081-afk <n221081@rguktn.ac.in>
2026-07-17 11:31:46 +0530

    fixed boundary condition

--- src/GradeCaluculator.java
+++ src/GradeCaluculator.java
@@ -4,16 +4,16 @@ public class GradeCaluculator {
         Scanner input = new Scanner(System.in);
         System.out.println("enter your marks:");
         int marks = input.nextInt();
-        if(marks>90){
+        if(marks>=90){
             System.out.println("grade A");
         }
-        else if(marks > 75){
+        else if(marks >= 75){
             System.out.println("grade B ");
         }
-        else if(marks>60){
+        else if(marks>=60){
             System.out.println("grade C");
         }
-        else if(marks >35){
+        else if(marks >=35){
             System.out.println("pass");
         }
         else{
