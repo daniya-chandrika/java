4e8fc9e69d9c30ea346efa921ba6ccd3812b6635
n221081-afk <n221081@rguktn.ac.in>
2026-07-17 10:07:35 +0530

    largest of two numbers

---
+++ src/LargestOfTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LargestOfTwo {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter first number:");
+        int first = input.nextInt();
+        System.out.println("enter second number:");
+        int second = input.nextInt();
+        if(first>second){
+            System.out.println("first number is largest:"+first);
+        }
+        else{
+            System.out.println("second number is largest:"+second);
+        }
+    }
+}
