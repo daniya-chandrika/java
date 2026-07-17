e1d8b5f1bbfcbc201f54c3f1a8d83ccac69d510b
n221081-afk <n221081@rguktn.ac.in>
2026-07-17 09:55:30 +0530

    evenorodd program

---
+++ src/evenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class evenOrOdd {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a number:" );
+        int number = input.nextInt();
+        if(number%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+    }
+}
---
+++ src/firstprogram.java
@@ -0,0 +1,5 @@
+public class firstprogram{
+    public static void main(String[] args){
+        System.out.println("Hello, I'm Ammu\nThis is the first day of my software journey ");
+    }
+}
