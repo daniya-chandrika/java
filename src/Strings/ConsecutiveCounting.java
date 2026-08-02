b35f32d999beaf4af8d1bf2c2c008b3ee13aca40
n221081-afk <n221081@rguktn.ac.in>
2026-08-02 22:31:54 +0530

    added string compression problem

--- src/Strings/ConsecutiveCounting.java
+++ src/Strings/ConsecutiveCounting.java
@@ -1,4 +1,29 @@
 package Strings;
-
+import java.util.Scanner;
 public class ConsecutiveCounting {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the string: ");
+        String text = input.nextLine();
+        StringBuilder result = new StringBuilder();
+        int length = text.length();
+        int count = 1;
+        for (int i = 0 ; i < length-1 ; i++){
+            if (text.isEmpty()) {
+                System.out.println("this is a empty string");
+                return;
+            }
+            if(text.charAt(i)==text.charAt(i+1)){
+                count++;
+            }else{
+                result.append(text.charAt(i));
+                result.append(count);
+                count = 1;
+            }
+        }
+        result.append(text.charAt(length-1));
+        result.append(count);
+        System.out.println(result);
+
+    }
 }
