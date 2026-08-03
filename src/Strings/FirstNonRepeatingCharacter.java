229891e131f4110d8ab2809b34b5077440920d1a
n221081-afk <n221081@rguktn.ac.in>
2026-08-03 22:25:14 +0530

    solved first non repeating character

--- src/Strings/FirstNonRepeatingCharacter.java
+++ src/Strings/FirstNonRepeatingCharacter.java
@@ -1,4 +1,35 @@
 package Strings;
-
+import java.util.Scanner;
 public class FirstNonRepeatingCharacter {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the string: ");
+        String text = input.nextLine();
+        if(text.trim().isEmpty()){
+            System.out.println("the string is empty");
+            return;
+        }
+        String trimmed = text.trim();
+        //char result=' ';
+        boolean found = false;
+        for(int i = 0 ; i< trimmed.length() ; i++){
+            char ch = trimmed.charAt(i);
+            int count = 0;
+            for(int j = 0 ; j < trimmed.length() ; j++){
+                if(ch == trimmed.charAt(j)){
+                    count++;
+                }
+            }
+            if(count==1){
+                //result= ch;
+                System.out.println("result is : "+ch);
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+            System.out.println("there is no first non repeating character");
+        }
+        input.close();
+    }
 }
