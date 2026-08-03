d4d9fca9e93e08e15674a965d51ac5f7de5b572f
n221081-afk <n221081@rguktn.ac.in>
2026-08-03 22:25:52 +0530

    solved reverse words problem

--- src/Strings/ReverseWords.java
+++ src/Strings/ReverseWords.java
@@ -1,4 +1,26 @@
 package Strings;
-
+import java.util.Scanner;
 public class ReverseWords {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the sentence: ");
+        String text = input.nextLine();
+        if (text.trim().isEmpty()) {
+            System.out.println("Given sentence is empty.");
+            return;
+        }
+        String [] words = text.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        //int length = words.length - 1;
+        //System.out.println(length);
+       for(int i = words.length -1; i >= 0; i--){
+            result.append(words[i]);
+            if(i!=0){
+                result.append(" ");
+            }
+        }
+       System.out.println("result: "+result);
+
+        input.close();
+    }
 }
