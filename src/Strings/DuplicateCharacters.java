12794e58a386490315558deedba5e31634862b3d
n221081-afk <n221081@rguktn.ac.in>
2026-08-03 22:20:34 +0530

    solved duplicate characters

---
+++ src/Strings/DuplicateCharacters.java
@@ -0,0 +1,26 @@
+package Strings;
+import java.util.Scanner;
+public class DuplicateCharacters {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the string: ");
+        String text = input.nextLine();
+        String trimmed = text.trim();
+        if(text.trim().isEmpty()){
+            System.out.println("given is empty string");
+            return;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0 ; i < trimmed.length(); i++){
+            char ch = trimmed.charAt(i);
+            if(trimmed.indexOf(ch)== i){
+                result.append(ch);
+            }
+            else{
+                continue;
+            }
+        }
+        System.out.println("result is : "+result);
+        input.close();
+    }
+}
