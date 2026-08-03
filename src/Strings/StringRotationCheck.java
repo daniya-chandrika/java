d4c39ce2b5142ed634442ac84f66dda511031035
n221081-afk <n221081@rguktn.ac.in>
2026-08-03 22:26:36 +0530

    solved string rotation check problem

--- src/Strings/StringRotationCheck.java
+++ src/Strings/StringRotationCheck.java
@@ -1,4 +1,23 @@
 package Strings;
-
+import java.util.Scanner;
 public class StringRotationCheck {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println(" enter the first string: ");
+        String text1 = input.nextLine();
+        System.out.println("enter the second string: ");
+        String text2 = input.nextLine();
+        if(text1.trim().length()!=text2.trim().length()){
+            System.out.println("lengths of the both strings are different ");
+            return;
+        }
+        String joinstring = text1+text1;
+        if(joinstring.contains(text2)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+        input.close();
+    }
 }
